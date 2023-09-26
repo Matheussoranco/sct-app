214c4acbec31107decd553acbb999291d71343c7
Matheus Soranço <matheussoranco@gmail.com>
2023-09-26 15:17:59 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootswatch/dist/flatly/bootstrap.css';
+import 'bootswatch/dist/lux/bootstrap.css';
 import 'toastr/build/toastr.min';
 import 'toastr/build/toastr.css';
 import Navbar from './components/navbar.js';
