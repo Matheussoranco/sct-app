a3c63871129a9b1a77a3cd478109b44f458f89ef
Matheus Soranço <matheussoranco@gmail.com>
2023-09-26 15:20:35 -0300

    Correção

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootswatch/dist/lux/bootstrap.css';
-import 'toastr/build/toastr.min';
-import 'toastr/build/toastr.css';
+//import 'toastr/build/toastr.min';
+//import 'toastr/build/toastr.css';
 import Navbar from './components/navbar.js';
 //import Rotas from './rotas.js';
 
