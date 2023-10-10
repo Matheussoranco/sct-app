8b3d4c2c5f780fbf99ddf1b89fded5e48883b1b6
Matheus Soranço <matheussoranco@gmail.com>
2023-10-10 14:02:05 -0300

    Criado listagem de salas e tipos de assentos

---
+++ src/views/listagem-tiposAssento.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+import Card from '../components/card';
+
+import { mensagemSucesso, mensagemErro } from '../components/toastr';
+
+import '../custom.css';
+
+import { useNavigate } from 'react-router-dom';
+
+import Stack from '@mui/material/Stack';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+import axios from 'axios';
+import { BASE_URL } from '../config/axios';
+
+const baseURL = `${BASE_URL}/TipoAssentos`;
+
+function ListagemAssentos() {
+  const navigate = useNavigate();
+
+  const cadastrar = () => {
+    navigate(`/cadastro-TipoAssento`);
+  };
+
+  const editar = (id) => {
+    navigate(`/cadastro-TipoAssento/${id}`);
+  };
+
+  const [dados, setDados] = React.useState(null);
+
+  async function excluir(id) {
+    let data = JSON.stringify({ id });
+    let url = `${baseURL}/${id}`;
+    console.log(url);
+    await axios
+      .delete(url, data, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(function (response) {
+        mensagemSucesso(`Tipo de assento excluído com sucesso!`);
+        setDados(
+          dados.filter((dado) => {
+            return dado.id !== id;
+          })
+        );
+      })
+      .catch(function (error) {
+        mensagemErro(`Erro ao excluir o Tipo de Assento`);
+      });
+  }
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setDados(response.data);
+    });
+  }, []);
+
+  if (!dados) return null;
+
+  return (
+    <div className='listContainer'>
+      <Card title='Listagem de assentos'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='bs-component'>
+              <button
+                type='button'
+                className='btn btn-warning'
+                onClick={() => cadastrar()}
+              >
+                Assentos disponíveis
+              </button>
+              <table className='table table-hover'>
+                <thead>
+                  <tr>
+                    <th scope='col'>Código</th>
+                    <th scope='col'>Nome</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dados.map((dado) => (
+                    <tr key={dado.id}>
+                      <td>{dado.id}</td>
+                      <td>{dado.disponivel}</td>
+                      <td>
+                        <Stack spacing={1} padding={0} direction='row'>
+                          <IconButton
+                            aria-label='edit'
+                            onClick={() => editar(dado.id)}
+                          >
+                            <EditIcon />
+                          </IconButton>
+                          <IconButton
+                            aria-label='delete'
+                            onClick={() => excluir(dado.id)}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </Stack>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>{' '}
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default ListagemTipoAssentos;
