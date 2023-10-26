369c8d76ad0b47100e18bf24201fd5cea61fdbae
Matheus Soranço <matheussoranco@gmail.com>
2023-10-26 13:53:28 -0300

    Correção de erros e montagem de combobox

--- src/views/cadastro/cadastro-salas.js
+++ src/views/cadastro/cadastro-salas.js
@@ -13,45 +13,37 @@ import '../custom.css';
 import axios from 'axios';
 import { BASE_URL } from '../config/axios';
 
-function CadastroAluno() {
+function CadastroSalas() {
   const { idParam } = useParams();
 
   const navigate = useNavigate();
 
-  const baseURL = `${BASE_URL}/alunos`;
+  const baseURL = `${BASE_URL}/salas`;
 
   const [id, setId] = useState('');
-  const [matricula, setMatricula] = useState(0);
-  const [nome, setNome] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [email, setEmail] = useState('');
-  const [celular, setCelular] = useState('');
-  const [idCurso, setIdCurso] = useState(0);
+  const [numSala, setNumSala] = useState('');
+  const [numAssentos, setNumAssentos] = useState('');
+  const [idCinema, setIdCinema] = useState(0);
+
 
   const [dados, setDados] = React.useState([]);
 
   function inicializar() {
     if (idParam == null) {
       setId('');
-      setMatricula(0);
-      setNome('');
-      setCpf('');
-      setEmail('');
-      setCelular('');
-      setIdCurso(0);
+      setNumSala('');
+      setNumAssentos('');
+      setIdCinema(0);
     } else {
       setId(dados.id);
-      setMatricula(dados.matricula);
-      setNome(dados.nome);
-      setCpf(dados.cpf);
-      setEmail(dados.email);
-      setCelular(dados.celular);
-      setIdCurso(dados.idCurso);
+      setNumSala(dados.numSala);
+      setNumAssentos(dados.numAssentos);
+      setIdCinema(dados.idCinema);
     }
   }
 
-  async function salvar() {
-    let data = { id, matricula, nome, cpf, email, celular, idCurso };
+  /*async function salvar() {
+    let data = { id, numSala, numAssentos, idCinema,};
     data = JSON.stringify(data);
     if (idParam == null) {
       await axios
@@ -59,8 +51,8 @@ function CadastroAluno() {
           headers: { 'Content-Type': 'application/json' },
         })
         .then(function (response) {
-          mensagemSucesso(`Aluno ${nome} cadastrado com sucesso!`);
-          navigate(`/listagem-alunos`);
+          mensagemSucesso(`Sala ${nome} cadastrada com sucesso!`);
+          navigate(`/adm/listagem-salas`);
         })
         .catch(function (error) {
           mensagemErro(error.response.data);
@@ -71,8 +63,8 @@ function CadastroAluno() {
           headers: { 'Content-Type': 'application/json' },
         })
         .then(function (response) {
-          mensagemSucesso(`Aluno ${nome} alterado com sucesso!`);
-          navigate(`/listagem-alunos`);
+          mensagemSucesso(`Sala ${numSala} alterada com sucesso!`);
+          navigate(`/adm/listagem-salas`);
         })
         .catch(function (error) {
           mensagemErro(error.response.data);
@@ -85,19 +77,16 @@ function CadastroAluno() {
       setDados(response.data);
     });
     setId(dados.id);
-    setMatricula(dados.matricula);
-    setNome(dados.nome);
-    setCpf(dados.cpf);
-    setEmail(dados.email);
-    setCelular(dados.celular);
-    setIdCurso(dados.idCurso);
-  }
+    setNumSala(dados.numSala);
+    setNumAssentos(dados.numAssentos);
+    setIdCinema(dados.idCinema);
+  }*/
 
-  const [dadosCursos, setDadosCursos] = React.useState(null);
+  const [dadosSalas, setDadosSalas] = React.useState(null);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/cursos`).then((response) => {
-      setDadosCursos(response.data);
+    axios.get(`${BASE_URL}/salas`).then((response) => {
+      setDadosSalas(response.data);
     });
   }, []);
 
@@ -106,83 +95,53 @@ function CadastroAluno() {
   }, [id]);
 
   if (!dados) return null;
-  if (!dadosCursos) return null;
+  if (!dadosSalas) return null;
 
   return (
     <div className='container'>
-      <Card title='Cadastro de Aluno'>
+      <Card title='Cadastro de Salas'>
         <div className='row'>
           <div className='col-lg-12'>
             <div className='bs-component'>
-              <FormGroup label='Matrícula: *' htmlFor='inputMatricula'>
+              <FormGroup label='Número da sala: *' htmlFor='inputNumSala'>
                 <input
                   type='text'
-                  id='inputMatricula'
-                  value={matricula}
+                  id='inputNumSala'
+                  value={numSala}
                   className='form-control'
-                  name='matricula'
-                  onChange={(e) => setMatricula(e.target.value)}
+                  name='NumSala'
+                  onChange={(e) => setNumSala(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Nome: *' htmlFor='inputNome'>
+              <FormGroup label='Numero de assentos: *' htmlFor='inputNumAssentos'>
                 <input
                   type='text'
-                  id='inputNome'
-                  value={nome}
+                  id='inputNumAssentos'
+                  value={numAssentos}
                   className='form-control'
-                  name='nome'
-                  onChange={(e) => setNome(e.target.value)}
+                  name='numAssentos'
+                  onChange={(e) => setNumAssentos(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='CPF: *' htmlFor='inputCpf'>
-                <input
-                  type='text'
-                  maxLength='11'
-                  id='inputCpf'
-                  value={cpf}
-                  className='form-control'
-                  name='cpf'
-                  onChange={(e) => setCpf(e.target.value)}
-                />
-              </FormGroup>
-              <FormGroup label='Email: *' htmlFor='inputEmail'>
-                <input
-                  type='email'
-                  id='inputEmail'
-                  value={email}
-                  className='form-control'
-                  name='email'
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </FormGroup>
-              <FormGroup label='Celular:' htmlFor='inputCelular'>
-                <input
-                  type='text'
-                  id='inputCelular'
-                  value={celular}
-                  className='form-control'
-                  name='celular'
-                  onChange={(e) => setCelular(e.target.value)}
-                />
-              </FormGroup>
-              <FormGroup label='Curso: *' htmlFor='selectCurso'>
+              <FormGroup label='Cinema:' htmlFor='selectCinema'>
                 <select
                   className='form-select'
-                  id='selectCurso'
-                  name='idCurso'
-                  value={idCurso}
-                  onChange={(e) => setIdCurso(e.target.value)}
+                  id='selectCinema'
+                  name='idCinema'
+                  value={idCinema}
+                  onChange={(e) => setIdCinema(e.target.value)}
                 >
                   <option key='0' value='0'>
                     {' '}
                   </option>
-                  {dadosCursos.map((dado) => (
+                  {dadosCinemas.map((dado) => (
                     <option key={dado.id} value={dado.id}>
                       {dado.nome}
                     </option>
                   ))}
                 </select>
               </FormGroup>
+              
               <Stack spacing={1} padding={1} direction='row'>
                 <button
                   onClick={salvar}
@@ -207,4 +166,4 @@ function CadastroAluno() {
   );
 }
 
-export default CadastroAluno;
+export default CadastroSalas;
